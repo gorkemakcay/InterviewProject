0c53959e71a979612d7fb50c100c6bf9385ef79b
Görkem Akçay <ggrkmakcy@gmail.com>
2023-01-04 23:55:06 +0300

    Add project files.

---
+++ MulakatProjesi/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MulakatProjesi.Models;
+
+namespace MulakatProjesi.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // ###########################################################
+
+    /// <summary>
+    /// İletişim sayfasına gider
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    /// <summary>
+    /// İletişim formu post metodu
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult Contact(ContactVM model)
+    {
+        return View();
+    }
+
+    // ###########################################################
+
+    /// <summary>
+    /// Ürünler sayfasına gider
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult Products()
+    {
+        return View();
+    }
+}
---
+++ MulakatProjesi/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    var productTable = $('#productTable').children('tbody');
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.kitapbulal.com/test/getproducts',
+        success: function (response) {
+            $.each(response, function (index, data) {
+                var image = `<td class="text-center align-middle" style="padding-top: 3px;"><img src="${data.imageUrl}" alt="${data.name}" width="50" /></td>`;
+                var name = `<td class="align-middle" style="font-size: 20px;">${data.name}</td>`;
+                var price = `<td class="text-center align-middle" style="font-size: 20px;">${data.price},00</td>`;
+
+                productTable.append(
+                    $('<tr>').append(image, name, price)
+                );
+            });
+        }
+    });
+});
